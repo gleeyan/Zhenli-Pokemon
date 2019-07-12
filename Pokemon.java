110545880291919802aced6ee3a72d86854ea5ce
gleeyan <25153775+gleeyan@users.noreply.github.com>
2019-07-12 17:28:39 -0500

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,38 @@
+package com.codingdojo.pokemon;
+
+public class Pokemon {
+	private String name;
+	private int health = 0;
+	private String type;
+	private static int count = 0;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health += health;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Pokemon(String name, String type, int health) {
+		setCount(getCount() + 1);
+		setName(name);
+		setType(type);
+		setHealth(health);
+	}
+	public static int getCount() {
+		return count;
+	}
+	public static void setCount(int count) {
+		Pokemon.count = count;
+	}
+}
